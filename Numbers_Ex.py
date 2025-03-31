961ef5a9e5149dc204d7ba952b60fc62450ac852
Juoan <95175079+Juoan@users.noreply.github.com>
2025-03-31 18:42:13 +0700

    Numbers_Ex.py

---
+++ Numbers_Ex.py
@@ -0,0 +1,37 @@
+#Ex1:
+num_one = 5
+num_two = 4
+#a) Add num_one and num_two and assign the value to a variable total
+total = num_one + num_two
+print(total)
+#b) Subtract num_two from num_one and assign the value to a variable diff
+diff = num_one - num_two
+print(diff)
+#c) Multiply num_two and num_one and assign the value to a variable product
+product = num_one * num_two
+print(product)
+#d) Divide num_one by num_two and assign the value to a variable division
+division = num_one / num_two
+print(division)
+#e) Use modulus division to find num_two divided by num_one and assign the value to a variable remainder
+remainder = num_two % num_one
+print(remainder)
+#f) Calculate num_one to the power of num_two and assign the value to a variable exp
+exp = num_one ** num_two
+print(exp)
+#g) Find floor division of num_one by num_two and assign the value to a variable floor_division
+floor_division = num_one // num_two
+print(floor_division)
+
+#Ex2:
+radius = 30
+#a) Calculate the area of a circle and assign the value to a variable named area_of_circle
+area_of_circle = 3.14 * radius ** 2
+print(area_of_circle)
+#b) Calculate the circumference of a circle and assign the value to a variable named circum_of_circle
+circum_of_circle = 2 * 3.14 * radius
+print(circum_of_circle)
+#c) Take radius as user input and calculate the area.
+radius = float(input("Enter the radius: "))
+area_of_circle = 3.14 * radius ** 2
+print(area_of_circle)
